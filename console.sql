418d02f613647ba3c70cbf750f4973d3ff962c5c
alineitgirl <queenalina2005@gmail.com>
2024-10-14 22:11:20 +0300

    Добавлены таблицы для отделов и должностей, для кода валюты, а также потсроена нормализация для всех баз данных

--- console.sql
+++ console.sql
@@ -1,17 +1,29 @@
+ create table departments (
+    id uuid primary key,
+    name varchar(100) not null unique 
+ );
+ 
+ create table positions(
+    id uuid primary key,
+    title varchar(100) not null unique 
+ );
+ 
 create table employees
 (
   id uuid primary key, 
   firstname varchar(50) not null,
   lastname varchar(50) not null,
-  date_of_birth date null,
+  date_of_birth timestamp null,
   adress varchar(100) null,
-  passport varchar(100) not null,
-  phone_number varchar(20) not null,
-  position varchar(30) not null,
-  department varchar(50),
+  passport varchar(100) not null unique ,
+  phone_number varchar(20) not null unique ,
+  position_id varchar(30) not null,
+  department_id varchar(50) not null ,
   salary numeric(10,2) not null,
   contract varchar(50) null ,
   age integer null
+  FOREIGN KEY (position_id) REFERENCES positions(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (department_id) REFERENCES departments(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 create table clients
@@ -19,11 +31,18 @@ create table clients
     id uuid primary key,
     firstname varchar(50) not null,
     lastname varchar(50) not null,
-    date_of_birth date null,
+    date_of_birth timestamp null,
     adress varchar(100) null,
-    passport varchar(100) not null,
-    phone_number varchar(20) not null,
+    passport varchar(100) not null unique,
+    phone_number varchar(20) not null unique ,
     age int null
+);
+
+CREATE TABLE currency_codes (
+    code CHAR(3) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    symbol VARCHAR(5),
+    country_region VARCHAR(100)
 );
 
 create table accounts
@@ -34,30 +53,68 @@ create table accounts
     amount money not null,
     client_id uuid not null ,
     FOREIGN KEY(client_id) REFERENCES clients(id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY(currency_code) REFERENCES currency_codes(code) ON DELETE CASCADE ON UPDATE CASCADE 
 );
 
-INSERT INTO employees (id, firstname, lastname, date_of_birth, adress, passport, phone_number, position, salary, age)
+INSERT INTO departments
+VALUES (
+    uuid_generate_v4(), 'IT-отдел'
+),
+(
+    uuid_generate_v4(), 'Отдел продаж'
+),
+(
+    uuid_generate_v4(), 'Финансовый отдел',
+),
+(
+    uuid_generate_v4(), 'HR'
+),
+(
+    uuid_generate_v4(), 'Руководство'
+);
+
+INSERT INTO positions 
+VALUES (
+    uuid_generate_v4(), 'backend-dev'
+),
+(
+    uuid_generate_v4(),'frontend-dev' 
+),
+(
+    uuid_generate_v4(), 'тестировщик'
+),
+(
+    uuid_generate_v4(), 'ux-дизайнер'
+),
+(
+    uuid_generate_v4(), 'dev-ops'
+),
+(
+    uuid_generate_v4(), 'бухгалтер'
+);
+
+INSERT INTO employees (id, firstname, lastname, date_of_birth, adress, passport, phone_number, position_id, department_id, salary, age)
 VALUES
 (
- uuid_generate_v4(), 'Сергей', 'Воробьев', '2001-01-03', 'г. Тирасполь', 'I-ПР234156', '77845635', 'backend-dev', 8500.67, 23
+ uuid_generate_v4(), 'Сергей', 'Воробьев', '2001-01-03', 'г. Тирасполь', 'I-ПР234156', '77845635', '271586c3-e205-4b52-8168-fcc21b8de274', '3b5ad8f6-aa1c-4c0f-a0ed-fd3e2c14aeda', 8500.67, 23
 ),
 (
- uuid_generate_v4(), 'Алексей', 'Скорцов', '1990-12-15', 'г. Бендеры', 'I-ПР294156', '77911233', 'frontend-dev', 6400.87, 34
+ uuid_generate_v4(), 'Алексей', 'Скорцов', '1990-12-15', 'г. Бендеры', 'I-ПР294156', '77911233', 'cc2ff737-2cf7-423e-a1b6-c5f7e2838b60','3b5ad8f6-aa1c-4c0f-a0ed-fd3e2c14aeda', 6400.87, 34
 ),
 (
- uuid_generate_v4(), 'Ольга', 'Пономарева', '2004-09-06', 'г. Тирасполь', 'I-ПР123456', '77799876', 'тестировщик', 5400.67, 20
+ uuid_generate_v4(), 'Ольга', 'Пономарева', '2004-09-06', 'г. Тирасполь', 'I-ПР123456', '77799876', '674f7e15-a217-4fe1-926f-5716821382d7','3b5ad8f6-aa1c-4c0f-a0ed-fd3e2c14aeda', 5400.67, 20
 ),
 (
- uuid_generate_v4(), 'Анастасия', 'Ковалева', '2005-03-12', 'г. Каменка', 'I-ПР234231', '77756432', 'ux-дизайнер', 6000.23, 19
+ uuid_generate_v4(), 'Анастасия', 'Ковалева', '2005-03-12', 'г. Каменка', 'I-ПР234231', '77756432', '3a19a180-81e5-402e-b1c3-8d4c5dea41f0', '3b5ad8f6-aa1c-4c0f-a0ed-fd3e2c14aeda', 6000.23, 19
 ),
 (
- uuid_generate_v4(), 'Александр', 'Трофимов', '1999-04-15', 'г. Тирасполь', 'I-ПР215678', '77734908', 'backend-dev', 8500.67, 25
+ uuid_generate_v4(), 'Александр', 'Трофимов', '1999-04-15', 'г. Тирасполь', 'I-ПР215678', '77734908', '271586c3-e205-4b52-8168-fcc21b8de274', '3b5ad8f6-aa1c-4c0f-a0ed-fd3e2c14aeda', 8500.67, 25
 ),
 (
- uuid_generate_v4(), 'Олег', 'Обручков', '2000-01-29', 'г. Бендеры', 'I-ПР128903', '77845452', 'dev-ops', 9000.67, 24
+ uuid_generate_v4(), 'Олег', 'Обручков', '2000-01-29', 'г. Бендеры', 'I-ПР128903', '77845452', 'bf82b317-74a8-4958-bf30-6d798217a5ed','3b5ad8f6-aa1c-4c0f-a0ed-fd3e2c14aeda', 9000.67, 24
 ),
 (
- uuid_generate_v4(), 'Алена', 'Гаврильченко', '1995-05-25', 'г. Тирасполь', 'I-ПР234763', '77788564', 'бухгалтер', 6000.50, 29
+ uuid_generate_v4(), 'Алена', 'Гаврильченко', '1995-05-25', 'г. Тирасполь', 'I-ПР234763', '77788564', 'f1397181-a81d-4bd6-a7fc-2979f5af8b6f','f4e161d6-b062-4851-84d3-b52347f79269', 6000.50, 29
 );
 
 INSERT INTO clients (id, firstname, lastname, date_of_birth, adress, passport, phone_number, age)
@@ -83,6 +140,28 @@ VALUES
 (
     uuid_generate_v4(), 'Николай', 'Иванов', '1999-09-25', 'г. Самара, ул. Победы, д. 9', 'MN789012', '79451112288', 25
 );
+
+INSERT INTO currency_codes (code, name, symbol, country_region) VALUES
+('USD', 'Доллар США', '$', 'США'),
+('EUR', 'Евро', '€', 'Еврозона'),
+('RUB', 'Российский рубль', '₽', 'Россия'),
+('GBP', 'Фунт стерлингов', '£', 'Великобритания'),
+('JPY', 'Японская иена', '¥', 'Япония'),
+('CHF', 'Швейцарский франк', '₣', 'Швейцария'),
+('CNY', 'Китайский юань', '¥', 'Китай'),
+('AUD', 'Австралийский доллар', '$', 'Австралия'),
+('CAD', 'Канадский доллар', '$', 'Канада'),
+('UAH', 'Гривна', '₴', 'Украина'),
+('KZT', 'Казахстанский тенге', '₸', 'Казахстан'),
+('BYN', 'Белорусский рубль', 'Br', 'Беларусь'),
+('TRY', 'Турецкая лира', '₺', 'Турция'),
+('INR', 'Индийская рупия', '₹', 'Индия'),
+('BRL', 'Бразильский реал', 'R$', 'Бразилия'),
+('ZAR', 'Южноафриканский рэнд', 'R', 'ЮАР'),
+('MXN', 'Мексиканское песо', '$', 'Мексика'),
+('KRW', 'Южнокорейская вона', '₩', 'Южная Корея'),
+('AED', 'Дирхам ОАЭ', 'د.إ', 'ОАЭ'),
+('RUP', 'Приднестровский рубль', 'р.', 'Приднестровье');
 
 INSERT INTO accounts
 VALUES
